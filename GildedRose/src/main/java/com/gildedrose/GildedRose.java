e6c89ef318a54d19a591fb8ffb63d9c94e4c4719
KillianRD <killdutilleul@gmail.com>
2025-03-05 09:04:42 +0100

    Refacto extract method

--- GildedRose/src/main/java/com/gildedrose/GildedRose.java
+++ GildedRose/src/main/java/com/gildedrose/GildedRose.java
@@ -13,51 +13,55 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            if (!item.name.equals(AgedBrie) && !item.name.equals(TAFKAL80ETC)) {
-                if (item.quality > 0) {
-                    if (!item.name.equals(Sulfuras)) {
-                        item.quality = item.quality - 1;
+            handleItem(item);
+        }
+    }
+
+    private static void handleItem(Item item) {
+        if (!item.name.equals(AgedBrie) && !item.name.equals(TAFKAL80ETC)) {
+            if (item.quality > 0) {
+                if (!item.name.equals(Sulfuras)) {
+                    item.quality = item.quality - 1;
+                }
+            }
+        } else {
+            if (item.quality < 50) {
+                item.quality = item.quality + 1;
+
+                if (item.name.equals(TAFKAL80ETC)) {
+                    if (item.sellIn < 11) {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1;
+                        }
                     }
+
+                    if (item.sellIn < 6) {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1;
+                        }
+                    }
+                }
+            }
+        }
+
+        if (!item.name.equals(Sulfuras)) {
+            item.sellIn = item.sellIn - 1;
+        }
+
+        if (item.sellIn < 0) {
+            if (!item.name.equals(AgedBrie)) {
+                if (!item.name.equals(TAFKAL80ETC)) {
+                    if (item.quality > 0) {
+                        if (!item.name.equals(Sulfuras)) {
+                            item.quality = item.quality - 1;
+                        }
+                    }
+                } else {
+                    item.quality = item.quality - item.quality;
                 }
             } else {
                 if (item.quality < 50) {
                     item.quality = item.quality + 1;
-
-                    if (item.name.equals(TAFKAL80ETC)) {
-                        if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-
-                        if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (!item.name.equals(Sulfuras)) {
-                item.sellIn = item.sellIn - 1;
-            }
-
-            if (item.sellIn < 0) {
-                if (!item.name.equals(AgedBrie)) {
-                    if (!item.name.equals(TAFKAL80ETC)) {
-                        if (item.quality > 0) {
-                            if (!item.name.equals(Sulfuras)) {
-                                item.quality = item.quality - 1;
-                            }
-                        }
-                    } else {
-                        item.quality = item.quality - item.quality;
-                    }
-                } else {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
-                    }
                 }
             }
         }
