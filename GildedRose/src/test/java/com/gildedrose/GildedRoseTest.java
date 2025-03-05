b8fb4f868c39b8c27e1dfd22d441ad2b41a37a27
KillianRD <killdutilleul@gmail.com>
2025-03-05 09:00:31 +0100

    Test 100% coverage

--- GildedRose/src/test/java/com/gildedrose/GildedRoseTest.java
+++ GildedRose/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -7,11 +7,78 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class GildedRoseTest {
 
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    void AgedBrie() {
+        Item[] items = new Item[] {
+                new Item("Aged Brie", 10, 40),
+                new Item("Aged Brie", 0, 40)
+        };
+
         GildedRose app = new GildedRose(items);
+
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+
+        assertEquals(41, app.items[0].quality);
+        assertEquals(9, app.items[0].sellIn);
+
+        assertEquals(42, app.items[1].quality);
+        assertEquals(-1, app.items[1].sellIn);
     }
 
+    @Test
+    void Sulfuras() {
+        Item[] items = new Item[] {
+                new Item("Sulfuras", 10, 40),
+                new Item("Sulfuras", 0, 40)
+        };
+
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals(39, app.items[0].quality);
+        assertEquals(9, app.items[0].sellIn);
+
+        assertEquals(38, app.items[1].quality);
+        assertEquals(-1, app.items[1].sellIn);
+    }
+
+    @Test
+    void TAFKAL80ETC() {
+        Item[] items = new Item[] {
+                new Item("Backstage passes to a TAFKAL80ETC concert", 10, 40),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 5, 40),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 0, 40)
+        };
+
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals(42, app.items[0].quality);
+        assertEquals(9, app.items[0].sellIn);
+
+        assertEquals(43, app.items[1].quality);
+        assertEquals(4, app.items[1].sellIn);
+
+        assertEquals(0, app.items[2].quality);
+        assertEquals(-1, app.items[2].sellIn);
+    }
+
+    @Test
+    void randomString() {
+        Item[] items = new Item[] {
+                new Item("foo", 10, 40),
+                new Item("Bonjour", 0, 40)
+        };
+
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals(39, app.items[0].quality);
+        assertEquals(9, app.items[0].sellIn);
+
+        assertEquals(38, app.items[1].quality);
+        assertEquals(-1, app.items[1].sellIn);
+    }
 }
