1cc80a0ed61eb5d061c99f6f4555f9d07dc86b77
KillianRD <killdutilleul@gmail.com>
2025-03-05 10:22:51 +0100

    Refactor pour s'adapter au polymorphisme

--- GildedRose/src/main/java/com/gildedrose/GildedRose.java
+++ GildedRose/src/main/java/com/gildedrose/GildedRose.java
@@ -1,10 +1,6 @@
 package com.gildedrose;
 
 class GildedRose {
-    private static final String TAFKAL80ETC = "Backstage passes to a TAFKAL80ETC concert";
-    private static final String AgedBrie = "Aged Brie";
-    private static final String Sulfuras = "Sulfuras, Hand of Ragnaros";
-
     Item[] items;
 
     public GildedRose(Item[] items) {
@@ -18,57 +14,7 @@ class GildedRose {
     }
 
     private static void handleItem(Item item) {
-        switch (item.name) {
-            case Sulfuras:
-                break;
-            case TAFKAL80ETC:
-                handleTAFKAL80ETC(item);
-                break;
-            case AgedBrie:
-                handleAgedBrie(item);
-                break;
-            default:
-                handleNormalItem(item);
-                break;
-        }
-    }
-
-    private static void handleTAFKAL80ETC(Item item) {
-        if (item.quality < 50) {
-            item.quality = item.quality + 1;
-
-            if (item.sellIn < 11)
-                item.quality = item.quality + 1;
-
-            if (item.sellIn < 6)
-                item.quality = item.quality + 1;
-        }
-
-        item.sellIn = item.sellIn - 1;
-
-        if (item.sellIn < 0)
-            item.quality = 0;
-
-    }
-
-    private static void handleAgedBrie(Item item) {
-        if (item.quality < 50)
-            item.quality = item.quality + 1;
-
-        item.sellIn = item.sellIn - 1;
-
-        if (item.sellIn < 0 && item.quality < 50)
-            item.quality = item.quality + 1;
-    }
-
-    private static void handleNormalItem(Item item) {
-        if (item.quality > 0)
-            item.quality = item.quality - 1;
-
-        item.sellIn = item.sellIn - 1;
-
-        if (item.sellIn < 0 && item.quality > 0)
-            item.quality = item.quality - 1;
+        item.updateItem();
     }
 }
 
