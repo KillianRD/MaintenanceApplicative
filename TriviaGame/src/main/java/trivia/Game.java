030c556ad257e772ec17ab3e9df30187f9655b0a
KillianRD <killdutilleul@gmail.com>
2025-03-06 08:51:22 +0100

    Erreur d'ortographe

--- TriviaGame/src/main/java/trivia/Game.java
+++ TriviaGame/src/main/java/trivia/Game.java
@@ -120,7 +120,7 @@ public class Game implements IGame {
    public boolean handleCorrectAnswer() {
       if (inPenaltyBox[currentPlayer]) {
          if (isGettingOutOfPenaltyBox) {
-            System.out.println("Answer was corrent!!!!");
+            System.out.println("Answer was correct!!!!");
             purses[currentPlayer]++;
             System.out.println(players.get(currentPlayer)
                                + " now has "
@@ -141,7 +141,7 @@ public class Game implements IGame {
 
       } else {
 
-         System.out.println("Answer was corrent!!!!");
+         System.out.println("Answer was correct!!!!");
          purses[currentPlayer]++;
          System.out.println(players.get(currentPlayer)
                             + " now has "
