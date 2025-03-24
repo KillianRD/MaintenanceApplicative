37d16f5dd3e885b006962a45500981ecc3edf012
KillianRD <killdutilleul@gmail.com>
2025-03-24 16:18:23 +0100

    Ajout des users

--- MyCalendar/src/main/java/com/mycalendar/model/User.java
+++ MyCalendar/src/main/java/com/mycalendar/model/User.java
@@ -2,16 +2,16 @@ package com.mycalendar.model;
 
 public class User {
 
-    private String name;
+    private String username;
     private String password;
 
     public User(String name, String password) {
-        this.name = name;
+        this.username = name;
         this.password = password;
     }
 
-    public String getName() {
-        return name;
+    public String getUsername() {
+        return username;
     }
 
     public boolean checkPassword(String password) {
